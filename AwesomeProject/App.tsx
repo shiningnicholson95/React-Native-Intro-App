d72da1dc7924947f00315246962f3225dff83701
ashish <abadoni47@gmail.com>
2019-08-30 09:41:27 +0530

    React App started successfully

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hey this is Ashish Badoni</Text>
+      <Text>This is a really cool app</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
